b8cd67de5a376e96292bed931e30e4d383ac641b
Steven Doran <78985334+S81D@users.noreply.github.com>
2023-05-04 15:06:33 -0500

    Add files via upload
    added a file containing commands to install the required python dependencies

---
+++ MCMC_reco/reco_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# some python dependencies that are needed for the main scripts
+# comment out the ones that already exist on your computer
+# (assumes that you have pip installed)
+
+# PDF fitting
+#python3 -m pip install statsmodels
+# or
+pip install statsmodels
+pip install pandas
+pip install scipy
+
+# emcee reco
+pip install corner
+pip install emcee
